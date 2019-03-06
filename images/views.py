a9a32b02359c064f39a591915f09cbd3c8ebd7ab
beattykariuki <bettykariuki026@gmail.com>
2019-03-06 12:01:31 +0300

    created a view function for the search category

--- images/views.py
+++ images/views.py
@@ -17,3 +17,15 @@ def app_category(request):
     categorys=Category.objects.all()
     return render (request,'welcome.html')
 
+def search_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"Results for: {search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term."
+        return render(request, 'all-images/search.html',{"message":message})
+

04ef23da593d128aa4c40e424b3f92dc6adea17a
beattykariuki <bettykariuki026@gmail.com>
2019-03-06 11:28:40 +0300

    created tests for deleting and saving images

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Category,Image,Location
 
 # Create your tests here.
+class Category_test(TestCase):
+    def setUp(self):
+        self.nature=Category(id=1,category_name="music")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nature,Category))
+
+    def test_save_category(self):
+        self.nature.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_category(self):
+        self.nature.delete_category()
+        category= Category.objects.all()
+        self.assertTrue(len(category) < 1)
